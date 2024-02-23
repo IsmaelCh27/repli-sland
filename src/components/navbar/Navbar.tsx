6d2cad1e68a128abd4eb50d6a13976d3a8d2640c
Ismael Choque <ismael2.7cc@gmail.com>
2024-02-22 23:41:19 -0400

    arreglo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Image from 'next/image';
 import logo from '../../../public/images/home/logo-header.png';
@@ -7,13 +7,14 @@ import logo from '../../../public/images/home/logo-header.png';
 import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <header className="z-30 flex h-[9.1rem] items-center justify-between bg-primary px-[7.29vw] text-[2rem] font-extralight lg:justify-start">
       <Link href="/" className="z-20">
         <Image src={logo} width={50} height={54} alt="" />
       </Link>
       <nav className="z-20 hidden  w-full lg:flex lg:items-center lg:justify-between">
-        <Menu />
+        <Menu setOpen={setOpen} />
       </nav>
 
       <MobileMenu />
